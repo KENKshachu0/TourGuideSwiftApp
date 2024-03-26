38b9cae81950d357b6a2ea1c3166ed7a118c901d
KENK <ken1178776360@icloud.com>
2024-03-26 20:58:58 +0800

    Initial Commit

---
+++ finalTourGuideSQL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ finalTourGuideSQL/finalTourGuideSQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalTourGuideSQLApp.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/3/26.
+//
+
+import SwiftUI
+
+@main
+struct finalTourGuideSQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
