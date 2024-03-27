fa4d6220673588df95f42f87f8eb6f0f400f1342
KENK <ken1178776360@icloud.com>
2024-03-27 20:08:12 +0800

    new

---
+++ finalTourGuideSQL/Views/TicketView.swift
@@ -0,0 +1,18 @@
+//
+//  TicketView.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/3/27.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TicketView()
+}
