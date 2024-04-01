319607d4355bd1d84875ca89fb0b66f348bee867
KENK <ken1178776360@icloud.com>
2024-04-01 18:21:53 +0800

    teacher toughed

---
+++ finalTourGuideSQL/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/3/27.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
