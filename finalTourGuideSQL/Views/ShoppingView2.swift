d86a67f2726d73424ccb975b6dfdf9a3698b36a1
KENK <ken1178776360@icloud.com>
2024-04-08 20:28:53 +0800

    shoppingview2 added

---
+++ finalTourGuideSQL/Views/ShoppingView2.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingView2.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/4/8.
+//
+
+import SwiftUI
+
+struct ShoppingView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingView2()
+}
