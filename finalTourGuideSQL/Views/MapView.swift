a6957fb68e3c0c7303dd6eebb5e666784dd7d58f
KENK <ken1178776360@icloud.com>
2024-04-10 21:29:42 +0800

    map added

---
+++ finalTourGuideSQL/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/4/10.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
