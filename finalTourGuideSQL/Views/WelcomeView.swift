475641a658f6acd9107e320995ba31856fd9e458
KENK <ken1178776360@icloud.com>
2024-04-18 22:29:46 +0800

    welcomeView added and pod added

---
+++ finalTourGuideSQL/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  finalTourGuideSQL
+//
+//  Created by KENK on 2024/4/18.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
